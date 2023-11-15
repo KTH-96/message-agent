7ce63ff908ccbc858741acd6e088fb60fa5b957f
KTH-96 <taehyun960@gmail.com>
2023-11-15 13:42:23 +0900

    test mock mvc를 이용해 Json 데이터 파싱 테스트 작성

--- api/src/test/java/com/kth/mssage/info/web/controller/util/JsonReader.java
+++ api/src/test/java/com/kth/mssage/info/web/controller/util/JsonReader.java
@@ -1,17 +1,12 @@
 package com.kth.mssage.info.web.controller.util;
 
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class JsonReader {
 
-    public static String readJsonFile(String fileName) throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource(fileName);
-        byte[] bytes = FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
-        return new String(bytes, StandardCharsets.UTF_8);
+    public static String TEST_ROOT_PATH = "C:\\Users\\TaeHyun\\IdeaProjects\\message-agent\\api\\src\\test\\resources\\";
+    public static String readJsonFile(String filePath) throws Exception {
+        return new String(Files.readAllBytes(Path.of(TEST_ROOT_PATH + filePath)));
     }
 }
